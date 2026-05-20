cb0f6c407f19683a0f545ca6543f2339a48b99ee
William Hicklin <venturehicklin@gmail.com>
2026-05-20 15:16:45 +0100

    Fixed code folling matter changes, matching latest rs-matter-embassy examples.

--- src/bin/rgb_lamp_wifi.rs
+++ src/bin/rgb_lamp_wifi.rs
@@ -5,88 +5,102 @@
 
 use core::pin::pin;
 
-use alloc::boxed::Box;
-
 use embassy_executor::Spawner;
 
 use esp_alloc::heap_allocator;
 use esp_backtrace as _;
-use esp_hal::analog::adc::{Adc, AdcConfig, Attenuation};
-use esp_hal::gpio::{Input, InputConfig, Pull};
+use esp_hal::ram;
 use esp_hal::timer::timg::TimerGroup;
-use esp_storage::FlashStorage;
+use esp_metadata_generated::memory_range;
 
 #[cfg(feature = "defmt")]
-use defmt::{error, info};
+use defmt::{info};
 #[cfg(feature = "log")]
-use log::{error, info};
+use log::{info};
+
+use rand_core::SeedableRng as _;
+use rs_matter_embassy::epoch::epoch;
+use rs_matter_embassy::matter::crypto::{default_crypto, Crypto};
+// Data Model imports
+use rs_matter_embassy::matter::dm::clusters::{
+    desc::{self, ClusterHandler as _},
+    app::level_control::{
+        self, AttributeDefaults, ClusterAsyncHandler as _, LevelControlHandler, OptionsBitmap,
+    },
+    app::on_off::{
+        self, ClusterAsyncHandler as _, OnOffHandler,
+    },
+};
+use rs_matter_embassy::matter::dm::devices::test::{DAC_PRIVKEY, TEST_DEV_ATT, TEST_DEV_COMM, TEST_DEV_DET};
+use rs_matter_embassy::matter::dm::{
+    Async, Dataver, DeviceType, EmptyHandler, Endpoint, EpClMatcher, Node,
+};
+
+use rs_matter_embassy::matter::tlv::Nullable;
+use rs_matter_embassy::matter::persist::DummyKvBlobStore;
+use rs_matter_embassy::matter::utils::init::InitMaybeUninit;
+use rs_matter_embassy::matter::{clusters, devices};
+use rs_matter_embassy::matter::dm::clusters::decl::color_control::ClusterHandler as _;
+use rs_matter_embassy::stack::rand::reseeding_csprng;
+use rs_matter_embassy::wireless::esp::EspWifiDriver;
+use rs_matter_embassy::wireless::{EmbassyWifi, EmbassyWifiMatterStack};
+
+use esp_hal::analog::adc::{Adc, AdcConfig, Attenuation};
+use esp_hal::gpio::{Input, InputConfig, Pull};
 
 use embassy_futures::select::{Either, Either3, select, select3};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::Channel;
 use embassy_time::Timer;
 
-use rs_matter_embassy::epoch::epoch;
-use rs_matter_embassy::matter::dm::clusters::desc::{self, ClusterHandler as _};
-use rs_matter_embassy::matter::dm::clusters::level_control::{
-    self, AttributeDefaults, ClusterAsyncHandler as _, LevelControlHandler, OptionsBitmap,
-};
-use rs_matter_embassy::matter::dm::clusters::on_off::{
-    self, ClusterAsyncHandler as _, OnOffHandler,
-};
-use rs_matter_embassy::matter::dm::devices::test::{TEST_DEV_ATT, TEST_DEV_COMM, TEST_DEV_DET};
-use rs_matter_embassy::matter::dm::{
-    Async, Dataver, DeviceType, EmptyHandler, Endpoint, EpClMatcher, Node,
-};
-
-use rs_matter_embassy::matter::tlv::Nullable;
-use rs_matter_embassy::matter::utils::init::InitMaybeUninit;
-use rs_matter_embassy::matter::{clusters, devices};
-use rs_matter_embassy::persist::EmbassyKvBlobStore;
-use rs_matter_embassy::rand::esp::{esp_init_rand, esp_rand};
-use rs_matter_embassy::stack::persist::KvBlobStore;
-use rs_matter_embassy::wireless::esp::EspWifiDriver;
-use rs_matter_embassy::wireless::{EmbassyWifi, EmbassyWifiMatterStack};
-
-use embassy_embedded_hal::adapter::BlockingAsync;
-
-use matter_rgb_lamp::dm::color_control::{self, ClusterHandler as _};
+use matter_rgb_lamp::dm::color_control;
 use matter_rgb_lamp::led::led_driver;
-
 use matter_rgb_lamp::led::led_handler::LedHandler;
+
+use static_cell::StaticCell;
+// LED setup
+use esp_hal_smartled::buffer_size_async;
+use esp_hal::rmt::PulseCode;
 
 extern crate alloc;
 
+macro_rules! mk_static {
+    ($t:ty) => {{
+        #[cfg(not(feature = "esp32"))]
+        {
+            static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+            STATIC_CELL.uninit()
+        }
+        #[cfg(feature = "esp32")]
+        alloc::boxed::Box::leak(alloc::boxed::Box::<$t>::new_uninit())
+    }};
+}
+
+const NUM_LEDS: usize = 1;
+
+/// The amount of memory for allocating all `rs-matter-stack` futures created during
+/// the execution of the `run*` methods.
+/// This does NOT include the rest of the Matter stack.
+///
+/// The futures of `rs-matter-stack` created during the execution of the `run*` methods
+/// are allocated in a special way using a small bump allocator which results
+/// in a much lower memory usage by those.
+///
+/// If - for your platform - this size is not enough, increase it until
+/// the program runs without panics during the stack initialization.
 const BUMP_SIZE: usize = 18000;
 
+/// Heap strictly necessary only for Wifi+BLE and for the only Matter dependency which needs (~4KB) alloc - `x509`
+#[cfg(not(feature = "esp32"))]
+const HEAP_SIZE: usize = 100 * 1024;
+/// On the esp32, we allocate the Matter Stack from heap as well, due to the non-contiguous memory regions on that chip
 #[cfg(feature = "esp32")]
-const HEAP_SIZE: usize = 40 * 1024; // 40KB for ESP32, which has a disjoint heap
-#[cfg(any(feature = "esp32c3", feature = "esp32h2"))]
-const HEAP_SIZE: usize = 160 * 1024;
-#[cfg(not(any(feature = "esp32", feature = "esp32c3", feature = "esp32h2")))]
-const HEAP_SIZE: usize = 186 * 1024;
+const HEAP_SIZE: usize = 140 * 1024;
+
+const RECLAIMED_RAM: usize =
+    memory_range!("DRAM2_UNINIT").end - memory_range!("DRAM2_UNINIT").start;
 
 esp_bootloader_esp_idf::esp_app_desc!();
-
-fn get_persistent_store() -> impl KvBlobStore {
-    use esp_bootloader_esp_idf::partitions::{
-        DataPartitionSubType, PARTITION_TABLE_MAX_LEN, PartitionType, read_partition_table,
-    };
-
-    let mut flash = FlashStorage::new();
-    let mut pt_mem = [0u8; PARTITION_TABLE_MAX_LEN];
-    let pt = read_partition_table(&mut flash, &mut pt_mem).unwrap();
-    let nvs = pt
-        .find_partition(PartitionType::Data(DataPartitionSubType::Nvs))
-        .unwrap()
-        .unwrap();
-
-    let start = nvs.offset();
-    let end = nvs.offset() + nvs.len();
-    info!("Found persistent partition at {:#x}..{:#x}", start, end);
-
-    EmbassyKvBlobStore::new(BlockingAsync::new(flash), start..end)
-}
 
 #[cfg(feature = "defmt")]
 use esp_println as _;
@@ -98,21 +112,13 @@ async fn main(_s: Spawner) {
 
     info!("Starting...");
 
-    // Heap strictly necessary only for Wifi+BLE and for the only Matter dependency which needs (~4KB) alloc - `x509`
-    // However since `esp32` specifically has a disjoint heap which causes bss size troubles, it is easier
-    // to allocate the statics once from heap as well
-    heap_allocator!(size: HEAP_SIZE);
-    #[cfg(feature = "esp32")]
-    heap_allocator!(#[link_section = ".dram2_uninit"] size: 96 * 1024);
+    heap_allocator!(size: HEAP_SIZE - RECLAIMED_RAM);
+    heap_allocator!(#[ram(reclaimed)] size: RECLAIMED_RAM);
 
     // == Step 1: ==
     // Necessary `esp-hal` and `esp-wifi` initialization boilerplate
 
     let peripherals = esp_hal::init(esp_hal::Config::default());
-
-    // To erase generics, `Matter` takes a rand `fn` rather than a trait or a closure,
-    // so we need to initialize the global `rand` fn once
-    esp_init_rand(esp_hal::rng::Rng::new());
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_rtos::start(
@@ -122,20 +128,23 @@ async fn main(_s: Spawner) {
             .software_interrupt0,
     );
 
-    let init = esp_radio::init().unwrap();
-
     // == Step 2: ==
     // Allocate the Matter stack.
     // For MCUs, it is best to allocate it statically, so as to avoid program stack blowups (its memory footprint is ~ 35 to 50KB).
     // It is also (currently) a mandatory requirement when the wireless stack variation is used.
-    let stack =
-        &*Box::leak(Box::new_uninit()).init_with(EmbassyWifiMatterStack::<BUMP_SIZE, ()>::init(
-            &TEST_DEV_DET,
-            TEST_DEV_COMM,
-            &TEST_DEV_ATT,
-            epoch,
-            esp_rand,
-        ));
+    let stack = mk_static!(EmbassyWifiMatterStack::<BUMP_SIZE, ()>).init_with(
+        EmbassyWifiMatterStack::init(&TEST_DEV_DET, TEST_DEV_COMM, &TEST_DEV_ATT, epoch),
+    );
+
+    // TODO: Change this to using Trng when it's got an API that dose not consume any peripherals that we need!
+    let mut seed = [0u8; 32];
+    esp_hal::rng::Rng::new().read(&mut seed);
+    let crypto = default_crypto(
+        reseeding_csprng(rand_chacha::ChaCha20Rng::from_seed(seed), 1000).unwrap(),
+        DAC_PRIVKEY,
+    );
+    let mut weak_rand = crypto.weak_rand().unwrap();
+
 
     // == Step 3: ==
     // Set up Matter data model handler
@@ -154,12 +163,12 @@ async fn main(_s: Spawner) {
     let led_handler = LedHandler::new(sender, button_on_off, adc1, pin);
 
     let on_off_handler = OnOffHandler::new(
-        Dataver::new_rand(stack.matter().rand()),
+        Dataver::new_rand(&mut weak_rand),
         LIGHT_ENDPOINT_ID,
         &led_handler,
     );
     let level_control_handler = LevelControlHandler::new(
-        Dataver::new_rand(stack.matter().rand()),
+        Dataver::new_rand(&mut weak_rand),
         LIGHT_ENDPOINT_ID,
         &led_handler,
         AttributeDefaults {
@@ -195,7 +204,7 @@ async fn main(_s: Spawner) {
             ),
             Async(
                 color_control::ColorControlHandler::new(
-                    Dataver::new_rand(stack.matter().rand()),
+                    Dataver::new_rand(&mut weak_rand),
                     &led_handler,
                 )
                 .adapt(),
@@ -203,43 +212,54 @@ async fn main(_s: Spawner) {
         )
         .chain(
             EpClMatcher::new(Some(LIGHT_ENDPOINT_ID), Some(desc::DescHandler::CLUSTER.id)),
-            Async(desc::DescHandler::new(Dataver::new_rand(stack.matter().rand())).adapt()),
+            Async(desc::DescHandler::new(Dataver::new_rand(&mut weak_rand)).adapt()),
         );
 
+
     // == Step 4: ==
+    // Create a KV BLOB store and load any previously saved state of `rs-matter`
+    // `SeqMapKvBlobStore` saves to a user-supplied NOR Flash region
+    // However, for this demo and for simplicity, we use a dummy KV BLOB store that does nothing
+    let mut kv = DummyKvBlobStore;
+    stack.startup(&crypto, &mut kv).await.unwrap();
+
+    // Wrap the KV BLOB store as a shared reference, so that it can be used both by `rs-matter` and the user
+    let kv = stack.create_shared_kv(kv).unwrap();
+
+    // == Step 5: ==
     // Run the Matter stack with our handler
     // Using `pin!` is completely optional, but reduces the size of the final future
-
-    // Create the persister & load any previously saved state
-    // `EmbassyPersist`+`EmbassyKvBlobStore` saves to a user-supplied NOR Flash region
-    // However, for this demo and for simplicity, we use a dummy persister that does nothing
-    let persist = stack
-        .create_persist_with_comm_window(get_persistent_store())
-        .await
-        .unwrap();
-
+    //
     // This step can be repeated in that the stack can be stopped and started multiple times, as needed.
     let mut matter = pin!(stack.run_coex(
         // The Matter stack needs to instantiate an `embassy-net` `Driver` and `Controller`
         EmbassyWifi::new(
-            EspWifiDriver::new(&init, peripherals.WIFI, peripherals.BT),
-            stack
+            EspWifiDriver::new(peripherals.WIFI, peripherals.BT),
+            weak_rand,
+            true, // Use a random BLE address
+            stack,
         ),
-        // The Matter stack needs a persister to store its state
-        &persist,
+        // The crypto provider
+        &crypto,
         // Our `AsyncHandler` + `AsyncMetadata` impl
         (NODE, handler),
-        // User future to run; the LED task
+        // The Matter stack needs a blob store to store its state
+        &kv,
+        // No user future to run; The LED task
         (),
     ));
 
-    // == Step 5: ==
+    // == Step 6: ==
     // Setup the LED driver
     let receiver = channel.receiver();
-    let led_driver = led_driver::Driver::new(peripherals.RMT, peripherals.GPIO8.into(), receiver);
+
+    static RMT_BUFFER: StaticCell<[PulseCode; buffer_size_async(NUM_LEDS)]> = StaticCell::new();
+    let rmt_buffer = RMT_BUFFER.init([PulseCode::default(); buffer_size_async(NUM_LEDS)]);
+
+    let led_driver = led_driver::Driver::new(peripherals.RMT, peripherals.GPIO8.into(), receiver, rmt_buffer);
     let mut led_task = pin!(led_driver.run());
 
-    // == Step 6: ==
+    // == Step 7: ==
     // Setup reset button
     let mut button_reset = Input::new(
         peripherals.GPIO9,
@@ -253,12 +273,7 @@ async fn main(_s: Spawner) {
             match select(button_reset.wait_for_rising_edge(), Timer::after_secs(3)).await {
                 Either::First(_) => (),
                 Either::Second(_) => {
-                    info!("Performing factory reset...");
-                    if let Err(e) = persist.reset().await {
-                        error!("Factory reset error1: {}", e);
-                    };
-                    // todo reset non-volatile attributes.
-                    // todo Consider adding a `reset()` method to the rs-matter handlers.
+                    info!("Factor reset has not been implemented");
                 }
             }
         }
@@ -290,18 +305,17 @@ const DEV_TYPE_ENHANCED_COLOR_LIGHT: DeviceType = DeviceType {
 
 /// The Matter Light device Node
 const NODE: Node = Node {
-    id: 0,
     endpoints: &[
         EmbassyWifiMatterStack::<0, ()>::root_endpoint(),
-        Endpoint {
-            id: LIGHT_ENDPOINT_ID,
-            device_types: devices!(DEV_TYPE_ENHANCED_COLOR_LIGHT),
-            clusters: clusters!(
+        Endpoint::new(
+            LIGHT_ENDPOINT_ID,
+            devices!(DEV_TYPE_ENHANCED_COLOR_LIGHT),
+            clusters!(
                 desc::DescHandler::CLUSTER,
                 OnOffHandler::<LedHandler, LedHandler>::CLUSTER,
                 LevelControlHandler::<LedHandler, LedHandler>::CLUSTER
                 color_control::ColorControlHandler::<LedHandler>::CLUSTER
             ),
-        },
+        ),
     ],
 };
